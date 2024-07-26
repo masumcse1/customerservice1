076e8782af6dadc87fe2396e35fdef5dfe01e292
masumcse1 <optinfra.masum@gmail.com>
2024-07-26 16:54:51 +0600

    correction.

--- src/test/java/com/opt/contoller/SaleControllerTest.java
+++ src/test/java/com/opt/contoller/SaleControllerTest.java
@@ -9,14 +9,19 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class SaleControllerTest {
@@ -29,11 +34,27 @@ public class SaleControllerTest {
 
     private MockMvc mockMvc;
 
+    private List<Object[]> topSellingItemsAllTime;
+    private List<Object[]> topSellingItemsLastMonth;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(saleController).build();
+
+
+        topSellingItemsAllTime = Arrays.asList(
+                new Object[]{"Item1", 1000.0},
+                new Object[]{"Item2", 800.0}
+        );
+
+        topSellingItemsLastMonth = Arrays.asList(
+                new Object[]{"Item1", 10L},
+                new Object[]{"Item2", 8L}
+        );
     }
+
+
 
     @Test
     public void testGetWishList() throws Exception {
@@ -56,10 +77,44 @@ public class SaleControllerTest {
 
 
     @Test
-    public void testGetTopSellingItemsOfAllTime() throws Exception {
+    public void testGetTopSellingItems() throws Exception {
         mockMvc.perform(get("/api/top-selling-items-all-time")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetTopSellingItemsOfAllTime() throws Exception {
+        when(saleService.getTopSellingItemsOfAllTime()).thenReturn(topSellingItemsAllTime);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/top-selling-items-all-time"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(topSellingItemsAllTime.size()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0][0]").value("Item1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0][1]").value(1000.0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1][0]").value("Item2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1][1]").value(800.0));
+    }
+
+    @Test
+    public void testGetTopSellingItemsOfLastMonth() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = sdf.parse("2024-06-01");
+        Date endDate = sdf.parse("2024-06-30");
+
+        when(saleService.getTopSellingItemsOfLastMonth(startDate, endDate)).thenReturn(topSellingItemsLastMonth);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/top-selling-items-last-month")
+                        .param("startDate", "2024-06-01")
+                        .param("endDate", "2024-06-30"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(topSellingItemsLastMonth.size()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0][0]").value("Item1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0][1]").value(10))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1][0]").value("Item2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1][1]").value(8));
     }
 
 
